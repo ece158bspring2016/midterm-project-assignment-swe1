08e2f754304b5c0cfb33f0a1662748072ff19569
Erik Xu <dixu@ucsd.edu>
2016-05-24 12:11:03 -0700

    - Added PIR sensor detection for exit

--- Raspberry Pi/rpi_client.py
+++ Raspberry Pi/rpi_client.py
@@ -16,41 +16,41 @@ except RuntimeError:
 GPIO.setmode(GPIO.BOARD)
 
 
-PIRPin         = 0            # Input Pin for PIR Sensor
-PIRStatus      = None         # PIR Sensor HIGH/LOW output
-occupancy_count = 0            # Variable to hold weight room occupancy
-firebase_update_interval = 10 # Push to Firebase every X occupants
+PIR_enter                = 0            # Input Pin for ENTER PIR Sensor
+PIR_exit                 = 0            # Input Pin for EXIT PIR Sensor
+PIRStatus                = None         # PIR Sensor HIGH/LOW output
+occupancy_enter          = 0            # Variable for amount of people entering
+occupancy_exit           = 0            # Variable for amount of people exiting
+firebase_update_interval = 10           # Push to Firebase every X occupants
 firebase_URL = Firebase('https://flickering-inferno-7283.firebaseio.com/message_list/')
 date = time.strftime('%m/%d/%Y')
 
 while (1):
     # Reset occupancy every day
     if date is not time.strftime('%m/%d/%Y'):
-        occupancy_count = 0
+        occupancy_enter = 0
+        occupancy_exit = 0
 
     # Wait (blocking) on GPIO Rise (Low to High)
-    GPIO.wait_for_edge(PIRPin, GPIO.RISING)
-    occupancy_count  += 1
+    GPIO.add_event_detect(PIR_enter, GPIO.RISING)  # add rising edge detection on a channel
+    GPIO.add_event_detect(PIR_exit, GPIO.RISING)  # add rising edge detection on a channel
+
+    if GPIO.event_detected(PIR_enter):
+        occupancy_enter += 1
+    if GPIO.event_detected(PIR_exit):
+        occupancy_exit += 1
+
 
     # Setup JSON for time, date, and current occupants
     data = {'time': time.strftime('%H:%M:%S'),
             'date': time.strftime('%m/%d/%Y'),
-            'occupancy': occupancy_count
+            'occupancyEnter': occupancy_enter,
+            'occupancyExit': occupancy_exit
            }
 
     # Push data to Firebase every X occupants
-    if occupancy_count  % firebase_update_interval == 0:
+    if (occupancy_enter  % firebase_update_interval == 0) or (occupancy_exit % firebase_update_interval == 0):
         result = firebase_URL.push(data)
         print result
 
-    # data1 = {'name': 'erikx',
-    #         'time': time.strftime('%H:%M:%S'),
-    #         'date': time.strftime('%m/%d/%Y'),
-    #         'occupancy': 0
-    #        }
 
-    #r = f.update(data1)
-    #print r
-
-    # r = firebase_URL.get()
-    # print json.dumps(r, sort_keys=True, indent=4, separators=(',', ': '))
